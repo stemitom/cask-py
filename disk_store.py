0426d5e015312eee450a5fd0b59982275b344ff8
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2023-10-27 22:15:19 +0100

    cask: add disk store and key dir functionalities

---
+++ disk_store.py
@@ -0,0 +1,56 @@
+import os.path
+import time
+
+from serializer import encode_kv, decode_kv
+from typing import Dict
+
+class KeyDir:
+    def __init__(self) -> None:
+        self.map: Dict[str, int] = {}
+    
+    def set_key_offset(self, key: str, offset: int) -> None:
+        self.map[key] = offset
+        
+    def get_offset(self, key: str) -> int:
+        return self.map.get(key, -1)
+
+
+class DiskStorage:
+    def __init__(self, file_name: str = "data.db"):
+        self.filename = file_name
+        self.file = open(os.path.abspath(self.filename), "ab+")
+        self._key_dir = KeyDir()
+    
+    def _write(self, data: bytes) -> int:
+        offset = self.file.tell()
+        self.file.write(data)
+        self.file.flush()
+        os.fsync(self.file.fileno())
+        return offset
+
+    def set(self, key: str, value: str) -> None:
+        timestamp = int(time.time())
+        _, data = encode_kv(timestamp, key, value)
+        offset = self._write(data)
+        self._key_dir.set_key_offset(key, offset)
+            
+    def get(self, key: str) -> str:
+        offset = self._key_dir.get_offset(key)
+        print(offset)
+        if offset != -1:
+            self.file.seek(offset)
+            encoded_data = self.file.read()
+            _, _, value = decode_kv(encoded_data)
+            return value
+        return ""
+
+    def close(self) -> None:
+        self.file.flush()
+        os.fsync(self.file.fileno())
+        self.file.close()
+
+    def __setitem__(self, key: str, value: str) -> None:
+        return self.set(key, value)
+
+    def __getitem__(self, item: str) -> str:
+        return self.get(item)
--- serializer.py
+++ serializer.py
@@ -6,7 +6,6 @@ HEADER_SIZE = 12
 def encode_header(timestamp: int, key_size: int, value_size: int) -> bytes:
     return struct.pack("<III", timestamp, key_size, value_size)
 
-
 def encode_kv(timestamp: int, key: str, value: str) -> tuple[int, bytes]:
     header: bytes = encode_header(timestamp, len(key), len(value))
     key_bytes: bytes = key.encode("utf-8")
@@ -24,5 +23,4 @@ def decode_kv(data: bytes) -> tuple[int, str, str]:
 
 def decode_header(data: bytes) -> tuple[int, int, int]:
     timestamp, key_size, value_size = struct.unpack("<III", data[:HEADER_SIZE])
-    print(len(data[:HEADER_SIZE]))
     return timestamp, key_size, value_size
